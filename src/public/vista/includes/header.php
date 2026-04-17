091e501a57c17b0a60c3021504dd9dc4624c5bed
larrysa123 <javierbarredaplanello@gmail.com>
2026-04-17 13:13:39 +0200

    correción Header

--- src/public/vista/includes/header.php
+++ src/public/vista/includes/header.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../../../app/config/config.php';
 
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">F
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 
     <link href="<?= RUTA_CSS ?>estilos.css" rel="stylesheet">
