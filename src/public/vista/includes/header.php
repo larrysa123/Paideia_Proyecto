a122317990c56bcad481ab03c1b6c050837f111a
larrysa123 <javierbarredaplanello@gmail.com>
2026-05-27 16:28:29 +0200

    arrelgar logo

--- src/public/vista/includes/header.php
+++ src/public/vista/includes/header.php
@@ -28,8 +28,8 @@ require_once __DIR__ . '/../../../app/config/config.php';
     <nav class="navbar navbar-expand-lg navbar-dark bg-paideia">
         <div class="container">
             <a class="navbar-brand fw-bold fs-3 d-flex align-items-center" href="<?= RUTA_INICIO ?>">
-                <img src="<?= BASE_URL ?>public/assets/img/logo.png" alt="Logo Paideía" class="logo-navbar">
-                Paideía
+                <img src="<?= BASE_URL ?>public/assets/img/logo-dorado.png" alt="Logo Paideía" class="logo-navbar">
+                <span class="texto-marca">Paideía</span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
