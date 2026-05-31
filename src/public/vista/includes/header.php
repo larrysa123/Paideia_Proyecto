eac91d17a84155a9023e8e608c1159f7722ca8cf
Javier <javierbarredaplanello@gmail.com>
2026-05-31 20:27:36 +0200

    Añadido logo en la pestaña del navegador (favicon) para mejorar la identidad del proyecto

--- src/public/vista/includes/header.php
+++ src/public/vista/includes/header.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/../../../app/config/config.php';
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
 
-
+    <link rel="icon" type="image/png" href="<?= RUTA_IMAGENES ?>favicon.png">
     <link href="<?= RUTA_CSS ?>estilos.css" rel="stylesheet">
 
     <script>
